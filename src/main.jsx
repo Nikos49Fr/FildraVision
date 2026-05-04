6c30d0274785e4b855317c93c43e170a5f427654
Nicolas Grignard <nicolas4980@gmail.com>
2026-05-04 18:23:38 +0200

    Bug : au refresh en prod, erreur 404 sauf page accueil. Test de correction avec HashRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import App from './App.jsx';
 import './styles/index.scss';
 import { ProfileProvider } from './context/profileContext';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
             <ProfileProvider>
                 <App />
             </ProfileProvider>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 );
